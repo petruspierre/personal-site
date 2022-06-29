9b3d38861d2e3be9d6a31b964e9fe1dc8646d6cc
Petrus Pierre <petrus.tsx@gmail.com>
2022-06-29 18:12:09 -0300

    refactor Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,31 @@
 import { Logo } from "./Logo"
 
-export const Header = () => {
+interface HeaderProps {
+  showNav?: boolean;
+}
+
+export const Header = (props: HeaderProps) => {
   return (
     <header className="w-full flex items-center justify-center p-2 py-8 max-w-5xl mx-auto">
-      <div className="w-[240px] md:w-full">
-        <Logo />
+      <div className="text-white h-full flex items-center gap-10">
+
+        {props.showNav && (
+          <a href="/blog" className="font-serif text-lg hover:underline">
+            blog
+          </a>
+        )}
+
+        <a href="/" className="w-[240px] md:w-full">  
+          <div>
+            <Logo />
+          </div>
+        </a>
+
+        {props.showNav && (
+          <a href="#contact" className="font-serif text-lg hover:underline">
+            contact
+          </a>
+        )}
       </div>
     </header>
   )
