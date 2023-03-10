9c98fcd273c199513e3fd9082504732d0538f103
Petrus Pierre <petrus.tsx@gmail.com>
2023-03-10 14:17:55 -0300

    add clarity tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -66,7 +66,7 @@ export default class MyDocument extends Document {
                       t.src="https://www.clarity.ms/tag/"+i;
                       y=l.getElementsByTagName(r)[0];
                       y.parentNode.insertBefore(t,y);
-                  })(window, document, "clarity", "script", "XXXXXXXXX");`,
+                  })(window, document, "clarity", "script", "g6j1fk6ghl");`,
             }}
           />
           ;
