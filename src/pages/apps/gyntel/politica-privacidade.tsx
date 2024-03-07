14a7bd8ab181ed4f5f89e5e22df4272002bec1f9
Petrus Pierre <petrus.tsx@gmail.com>
2024-03-07 12:52:09 -0300

    politica de privacidade gyntel

---
+++ src/pages/apps/gyntel/politica-privacidade.tsx
@@ -0,0 +1,62 @@
+export default function PoliticaGyntel() {
+  return (
+    <main className="w-full max-w-5xl flex-1 px-4 mx-auto py-4 text-white space-y-2 text-justify text-lg">
+      <h1 className="text-xl font-bold">Política Privacidade</h1>
+      <p>
+        A sua privacidade é importante para nós. É política da noc.ton respeitar
+        a sua privacidade em relação a qualquer informação sua que possamos
+        coletar no app Gyntel, e outros sites/aplicativoss que possuímos e
+        operamos.
+      </p>
+      <p>
+        Solicitamos informações pessoais apenas quando realmente precisamos
+        delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais,
+        com o seu conhecimento e consentimento. Também informamos por que
+        estamos coletando e como será usado.
+      </p>
+
+      <p>
+        Apenas retemos as informações coletadas pelo tempo necessário para
+        fornecer o serviço solicitado. Quando armazenamos dados, protegemos
+        dentro de meios comercialmente aceitáveis ​​para evitar perdas e roubos,
+        bem como acesso, divulgação, cópia, uso ou modificação não autorizados.
+      </p>
+
+      <p>
+        Não compartilhamos informações de identificação pessoal publicamente ou
+        com terceiros, exceto quando exigido por lei.
+      </p>
+
+      <p>
+        O nosso site pode ter links para sites externos que não são operados por
+        nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas
+        desses sites e não podemos aceitar responsabilidade por suas respectivas
+        políticas de privacidade.
+      </p>
+
+      <p>
+        Você é livre para recusar a nossa solicitação de informações pessoais,
+        entendendo que talvez não possamos fornecer alguns dos serviços
+        desejados.
+      </p>
+
+      <p>
+        O uso continuado de nosso site será considerado como aceitação de nossas
+        práticas em torno de privacidade e informações pessoais. Se você tiver
+        alguma dúvida sobre como lidamos com dados do usuário e informações
+        pessoais, entre em contato conosco.
+      </p>
+
+      <h2 className="font-bold pt-4">Mais informações</h2>
+
+      <p>
+        Esperemos que esteja esclarecido e, como mencionado anteriormente, se
+        houver algo que você não tem certeza se precisa ou não, geralmente é
+        mais seguro deixar os cookies ativados, caso interaja com um dos
+        recursos que você usa em nosso site.
+      </p>
+
+      <small>Esta política é efetiva a partir de Agosto/2020.</small>
+    </main>
+  );
+}
